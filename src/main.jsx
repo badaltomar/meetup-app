a2d20f5b7214ed34519afe64f34303218e229b71
Badal <shooterbadal@gmail.com>
2025-09-01 04:42:05 +0530

    Temporary rename for case fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import EventDetails from "./pages/eventDetails.jsx";
+import EventDetails from "./pages/eventDetailstemp.jsx";
 
 const router = createBrowserRouter([
   {
